58321bc163d5e55edbba4de71323839031059d1b
tehpsalmist <bstewardcodes@gmail.com>
2020-10-29 09:28:38 -0400

    adjust imports for parcel 2

--- src/graphql/mutations.ts
+++ src/graphql/mutations.ts
@@ -1,4 +1,4 @@
-import { gql } from '@apollo/client';
+import gql from 'graphql-tag'
 
 export const CREATE_WORKSPACE = gql`
   mutation CreateWorkspace ($name: String!) {
--- src/graphql/subscriptions.ts
+++ src/graphql/subscriptions.ts
@@ -1,4 +1,4 @@
-import { gql } from '@apollo/client'
+import gql from 'graphql-tag'
 
 export const FRENZINE_FORMS = gql`
   subscription FrenzineFormsSub ($workspaceId: Int!) {
